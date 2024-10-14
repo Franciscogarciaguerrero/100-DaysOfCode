7b14b8189ffcafde56644e66baf8d904830eb9fe
Fran <franciscogarciadev@gmail.com>
2024-10-14 21:34:09 +0200

    Update day3.py

--- days 1-10/day3.py
+++ days 1-10/day3.py
@@ -3,5 +3,8 @@ plant = input("What is your favorite plant? ")
 cooking = input("What is your favorite method of cooking?")
 burned = input("What is a word to describe burned food?")
 item = input("What is your favorite household item?")
+print()
 
-print(cooking, food, "with", burned, plant, "on a bed of", item)
+print("Tonight's dinner:")
+
+print("For dinner u should make", cooking, food, "with", burned, plant, "on a plate of", item)
